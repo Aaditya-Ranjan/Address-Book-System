53a2b89c96eec56d54a01be94e97e36a44ad139f
Aaditya Ranjan <ranjanaadi50@gmail.com>
2026-03-02 12:21:21 +0530

    [Aaditya] Added: UC1: Created ContactPerson class with all fields

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -3,5 +3,13 @@ package com.addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
+        ContactPerson contact = new ContactPerson(
+                "John", "Doe",
+                "123 Main St", "New York", "NY", "10001",
+                "9876543210", "john.doe@email.com"
+        );
+
+        System.out.println("Contact Created: " + contact.firstName + " " + contact.lastName);
     }
 }
---
+++ src/com/addressbook/ContactPerson.java
@@ -0,0 +1,25 @@
+package com.addressbook;
+
+public class ContactPerson {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+    String email;
+
+    public ContactPerson(String firstName, String lastName, String address,
+                         String city, String state, String zip,
+                         String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
